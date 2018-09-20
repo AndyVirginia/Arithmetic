e201a9c004e4e2ed42e0df03678dd7b197870175
3116005154 <1049955895@qq.com>
2018-09-20 19:11:49 +0800

    完整实现无括号下的算式生成器

--- generator.py
+++ generator.py
@@ -5,14 +5,14 @@ import math
 class Operator():
     '''随机生成一个算数运算符的列表'''
     def __init__(self):
-        operator = ['+', '-', '×', '÷', None]
+        operator = ['+', '-', '×', '÷', 'null']
         self.str = []
         for x in range(3):
             if x == 1:
                 self.str.append(random.choice(operator[:-2]))
             else:
                 x = random.choice(operator)
-                if x:
+                if x != 'null':
                     self.str.append(x)
 
 
@@ -20,7 +20,7 @@ class Num():
     '''生成随机数'''
     def creat_num(self, max_num):
         # 假分数时的分子,做乘除法运算有用
-        self.full_numerator = random.randint(0, max_num ** 2)   
+        self.full_numerator = random.randint(0, self.denominator * max_num)   
         # 带分数的整数部分
         self.inter = math.floor(self.full_numerator / self.denominator) 
         # 带分数的分子
@@ -44,6 +44,13 @@ class Num():
             self.str = str(self.numerator) +'/'+ str(self.denominator)
         else:
             self.str = str(self.inter) +'\''+ str(self.numerator) +'/'+ str(self.denominator)
+    
+    def zero(self):
+        '''判零函数'''
+        if self.full_numerator == 0 and self.inter == 0:
+            return True
+        else:
+            return False
 
 
 class AriExpression():
@@ -51,13 +58,21 @@ class AriExpression():
     def create_nums(self, max_num):
         '''创建运算数的列表'''
         self.nums = []
-        for x in range(len(self.operator.str)+1):
-            new_numbers = Num(max_num)
-            self.nums.append(new_numbers.str)
+        self.nums.append(Num(max_num))
+        for x in range(len(self.operator.str)):
+            new_num = Num(max_num)
+            if self.operator.str[x] == '÷':
+                while new_num.zero():
+                    new_num = Num(max_num)
+            self.nums.append(new_num)
     
     def create_expression(self):
-        '''创建表达式的字符串表示或表达式的'''
-        pass
+        '''创建表达式的字符串表示'''
+        self.exp = ''
+        for x in range(len(self.operator.str)):
+            self.exp += self.nums[x].str + ' '
+            self.exp += self.operator.str[x] + ' '
+        self.exp += self.nums[-1].str + ' ='
 
     def __init__(self, max_num):
         self.operator = Operator()
@@ -65,12 +80,6 @@ class AriExpression():
         self.create_expression()
 
 
-ari = AriExpression(10)
-for x in ari.operator.str:
-    print(x)
-for x in ari.nums:
-    print(x)
-
-
-    
-
+for x in range(100):
+    ari = AriExpression(10)
+    print(ari.exp)
