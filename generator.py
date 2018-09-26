db833b2e444e6924caba118db1b6de3669dd522a
3116005154 <1049955895@qq.com>
2018-09-26 21:10:59 +0800

    引入了Fraction类，并加入了插入括号的功能

--- generator.py
+++ generator.py
@@ -1,85 +1,98 @@
+from fractions import Fraction
 import random
-import math
 
 
-class Operator():
-    '''随机生成一个算数运算符的列表'''
-    def __init__(self):
+class Ari_Expression():
+    '''算术表达式的生成'''
+    def __init__(self, max_num):
+        self.init_operators()
+        self.init_nums(max_num)
+        self.init_expression()
+
+    def init_num(self, max_num):
+        '''随机生成数'''
+        denominator = random.randint(1, max_num)
+        numerator = random.randint(0, max_num*denominator)
+        return Fraction(numerator, denominator)
+
+    def insert_bracket(self):
+        '''插入括号'''
+        bracket = ['(', 'null', ')']
+        if len(self.operators) > 1:
+            x = random.randint(0, len(self.operators))
+            while x < len(self.operators):
+                y = random.randint(x, len(self.operators))
+                low = False
+                for a in self.operators[x:y+1]:
+                     if a in ['+', '-']:
+                         low = True
+                         break
+                try:
+                    if self.operators[y+1] in ['×', '÷'] and low:
+                        self.operators.insert(x, '(')
+                        self.operators.insert(y+2,')')
+                except IndexError:
+                    pass
+                x = y+2
+            
+
+    def init_operators(self):
+        '''随机生成一个运算符并随机插入括号'''
+        self.operators = []
         operator = ['+', '-', '×', '÷', 'null']
-        self.str = []
         for x in range(3):
             if x == 1:
-                self.str.append(random.choice(operator[:-2]))
+                self.operators.append(random.choice(operator[:-2]))
             else:
-                x = random.choice(operator)
-                if x != 'null':
-                    self.str.append(x)
-
-
-class Num():
-    '''生成随机数'''
-    def creat_num(self, max_num):
-        # 假分数时的分子,做乘除法运算有用
-        self.full_numerator = random.randint(0, self.denominator * max_num)   
-        # 带分数的整数部分
-        self.inter = math.floor(self.full_numerator / self.denominator) 
-        # 带分数的分子
-        self.numerator = self.full_numerator % self.denominator      
-
-    def __init__(self, max_num):
-        '''初始化方法'''
-        self.denominator = random.randint(1, max_num)    # 分母
-        self.creat_num(max_num)
-        self.output_str()
-
-    def output_str(self):
-        '''创建随机数的字符串形式'''
-        if self.numerator == 0:
-            # 分子等于0时
-            if self.inter == 0:
-                self.str = '0'
-            else:
-                self.str = str(self.inter)
-        elif self.inter == 0:
-            self.str = str(self.numerator) +'/'+ str(self.denominator)
-        else:
-            self.str = str(self.inter) +'\''+ str(self.numerator) +'/'+ str(self.denominator)
+                y = random.choice(operator)
+                if y != 'null':
+                    self.operators.append(y)
+        self.insert_bracket()
     
-    def zero(self):
-        '''判零函数'''
-        if self.full_numerator == 0 and self.inter == 0:
-            return True
-        else:
-            return False
-
-
-class AriExpression():
-    ''' 算术表达式的生成 '''
-    def create_nums(self, max_num):
-        '''创建运算数的列表'''
+    def init_nums(self, max_num):
         self.nums = []
-        self.nums.append(Num(max_num))
-        for x in range(len(self.operator.str)):
-            new_num = Num(max_num)
-            if self.operator.str[x] == '÷':
-                while new_num.zero():
-                    new_num = Num(max_num)
-            self.nums.append(new_num)
+        self.nums.append(self.init_num(max_num))
+        for x in range(len(self.operators)):
+            y = self.init_num(max_num)
+            if self.operators[x] == '÷':
+                while y.numerator == 0:
+                    y = self.init_num(max_num)
+            self.nums.append(y)
     
-    def create_expression(self):
-        '''创建表达式的字符串表示'''
-        self.exp = ''
-        for x in range(len(self.operator.str)):
-            self.exp += self.nums[x].str + ' '
-            self.exp += self.operator.str[x] + ' '
-        self.exp += self.nums[-1].str + ' ='
-
-    def __init__(self, max_num):
-        self.operator = Operator()
-        self.create_nums(max_num)
-        self.create_expression()
-
-
-for x in range(100):
-    ari = AriExpression(10)
-    print(ari.exp)
+    def str_num(self, num):
+        '''字符串化一个分数'''
+        inter = int(num.numerator / num.denominator)
+        numerator = int(num.numerator % num.denominator)
+        str_num = ''
+        if numerator != 0:
+            str_num += str(numerator) + '/' + str(num.denominator)
+        if not str_num:
+            '''如果为空'''
+            str_num += str(inter)
+        else:
+            if inter == 0:
+                return str_num
+            else:
+                str_num = str(inter) + '`' + str_num
+        return str_num
+    
+    def init_expression(self):
+        '''生成一个算术表达式的字符串形式'''
+        self.str = ''
+        i = 0
+        again = False
+        for x in self.operators:
+            if again:
+                self.str += x
+            elif x == '(':
+                self.str += x
+            elif x == ')':
+                self.str += self.str_num(self.nums[i])
+                i += 1
+                self.str += x
+                again = True
+            else:
+                self.str += self.str_num(self.nums[i])
+                self.str += x 
+                i += 1
+        self.str += self.str_num(self.nums[-1]) + ' ='
